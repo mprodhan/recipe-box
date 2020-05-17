2cf92417f0749991b24ea6f96ddbd8fbaa297981
Kathy Tran <44452243+katran009@users.noreply.github.com>
2020-05-16 19:45:52 -0500

    Update urls.py
    Adding login, logout, editrecipe, favorite, unfavorite & admin path

--- info/urls.py
+++ info/urls.py
@@ -3,10 +3,15 @@ from django.urls import path
 from info import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.home_index, name="homepage"),
     path('author/<int:author_id>/', views.author),
     path('recipe/<int:recipe_id>', views.recipe),
     path('recipeadd/', views.recipe_add_view),
     path('authoradd/', views.author_add_view),
-    path('login/', views.loginview)
+    path('login/', views.login_view, name="login"),
+    path('logout/', views.logout_view, name="logout"),
+    path('editrecipe/<int:recipe_id>', views.edit_recipe, name="edit"),
+    path('favorite/<int:id>/', views.favoriterecipe),
+    path('unfavorite/<int:id>/', views.unfavoriterecipe),
 ]

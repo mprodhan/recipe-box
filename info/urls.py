d8b278dec6676e2ea1ab0117e25a06ecb7755ec4
Kathy Tran <44452243+katran009@users.noreply.github.com>
2020-05-16 20:56:24 -0500

    Add files via upload

--- info/urls.py
+++ info/urls.py
@@ -4,11 +4,12 @@ from info import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home_index, name="homepage"),
-    path('author/<int:author_id>/', views.author),
+    path('', views.recipe, name="homepage"),
+    path('author/<str:author_name>', views.author, name="author"),
     path('recipe/<int:recipe_id>', views.recipe),
-    path('recipeadd/', views.recipe_add_view),
-    path('authoradd/', views.author_add_view),
+    path('newAuthor/', views.new_author, name="create author"),
+    path('newRecipe/', views.new_recipe, name="create recipe"),
+    path('newUser/', views.signup_view, name='create user'),
     path('login/', views.login_view, name="login"),
     path('logout/', views.logout_view, name="logout"),
     path('editrecipe/<int:recipe_id>', views.edit_recipe, name="edit"),
